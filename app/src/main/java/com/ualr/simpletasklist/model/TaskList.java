84d04206156aca8b297b1b53e88d8c8942cc6e8a
irconde <irconde@ualr.edu>
2020-08-02 22:21:14 -0500

    Typos fixed

--- app/src/main/java/com/ualr/simpletasklist/model/TaskList.java
+++ app/src/main/java/com/ualr/simpletasklist/model/TaskList.java
@@ -10,17 +10,17 @@ public class TaskList {
 
     // TODO 04. Define the class constructor and the corresponding getters and setters.
 
-    // TODO 06.03. Define a new methods called "add" that, given a task description, will create a
+    // TODO 06.03. Define a new method called "add" that, given a task description, will create a
     //  new task and add it to the task list.
 
     // TODO 06.04. Define a new "toString" method that provides a formatted string with all the tasks in the task list.
     // Format: 1 line per task. Each line should start with the id number of the task, then a dash, and the task description right after that.
     // If the task is marked as done, "Done" should be included at the end of the line
 
-    // TODO 07.03. Define a new methods called "delete" that, given a task id, will delete the
+    // TODO 07.03. Define a new method called "delete" that, given a task id, will delete the
     //  corresponding task from the task list.
 
-    // TODO 08.03. Define a new methods called "markDone" that, given a task id, will mark the
+    // TODO 08.03. Define a new method called "markDone" that, given a task id, will mark the
     //  corresponding task as done.
 
 }
